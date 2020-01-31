bf82c9606ebed1cfc26258cf1c0e8e96be26d03a
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-01-31 10:31:52 -0600

    add book form

---
+++ src/components/AddBook.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+class AddBook extends Component {
+  render() {
+    //console.log(this.props);
+    return (
+      <form id="add-book">
+        <div className="field">
+          <label>Book name:</label>
+          <input type="text" />
+        </div>
+
+        <div className="field">
+          <label>Genre:</label>
+          <input type="text" />
+        </div>
+
+        <div className="field">
+          <label>Author:</label>
+          <select>
+            <option>Select author</option>
+          </select>
+        </div>
+
+        <button></button>
+      </form>
+    );
+  }
+}
+
+export default graphql(getBooksQuery)(BookList);
